42783e84b42f359ecbb9476e23090149ebfceee9
Mimoune <djouallah@users.noreply.github.com>
2019-10-04 16:51:26 +1000

    Add files via upload

--- index.js
+++ index.js
@@ -4,34 +4,41 @@ const viz = require('@google/dscc-scripts/viz/initialViz.js');
 const local = require('./localMessage.js');
 const {Deck} = require ('@deck.gl/core');
 const  {ScatterplotLayer} = require('@deck.gl/layers');
-// to deply change to false
-export const LOCAL = true;
+
+// define the initial Canvas
+var canvasElement = document.createElement('canvas');
+var ctx = canvasElement.getContext('2d');
+canvasElement.id = "container";
+document.body.appendChild(canvasElement);
 
 
-const drawViz = (data) => {
+// change to false to deploy
+export const LOCAL = false;
+   
+const drawViz = (data) => { 
+document.body.innerHTML = "";
+var canvasElement = document.createElement('canvas');
+var ctx = canvasElement.getContext('2d');
+canvasElement.id = "container";
 
+// get can vas dimensions 
+var height = dscc.getHeight();
+var width = dscc.getWidth();
 
-  // create a canvas
-  var canvasElement = document.createElement('canvas');
-  var ctx = canvasElement.getContext('2d');
-  // clear the canvas.
-  ctx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-  // set the canvas width and height
-  var height = dscc.getHeight();
-  var width = dscc.getWidth();
-  ctx.canvas.width = dscc.getWidth();
-  ctx.canvas.height = dscc.getHeight();
+// clear canvas does not work
+// ctx.clearRect(0, 0, width, height);
+// ctx.beginPath();   
   // Deckgl code
-  var data1 = data.tables.DEFAULT;
-  var data2 = JSON.stringify(data1);
-  var data3 = data2.replace(/\"]/g, "]");
-  var data4 = data3.replace(/\["/g, "[");
-  var data4 = JSON.parse(data4);
+var data1 = data.tables.DEFAULT;
+var data2 = JSON.stringify(data1);
+var data3 = data2.replace(/\"]/g, "]");
+var data4 = data3.replace(/\["/g, "[");
+var data4 = JSON.parse(data4);
   // get intial Vaues for view
- var firstvalue = data1[0].coordinateid.toString();
- var coordinates = firstvalue.split(",");
- var longitudeView =parseFloat(coordinates[0]);
- var latitudeView =parseFloat(coordinates[1]);
+var firstvalue = data1[0].coordinateid.toString();
+var coordinates = firstvalue.split(",");
+var longitudeView =parseFloat(coordinates[0]);
+var latitudeView =parseFloat(coordinates[1]);
   
   const INITIAL_VIEW_STATE = {
     bearing: 0,
@@ -43,6 +50,9 @@ const drawViz = (data) => {
     pitch: 40.5,
   };
 new Deck({
+  width: width,
+  height: height,
+  
   initialViewState: INITIAL_VIEW_STATE,
   controller: true,
   layers: [
@@ -53,13 +63,12 @@ new Deck({
       getFillColor: d => d.colorid,
     })
   ],
-  
 });
 };
 // renders locally
 if (LOCAL) {
-  drawViz(local.message);
   
+  drawViz(local.message);
 } else {
   dscc.subscribeToData(drawViz, {transform: dscc.objectTransform});
 }
