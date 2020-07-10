ce9c3cb6454e697a20f67102ca60ba27195368dd
Mimoune <djouallah@users.noreply.github.com>
2020-07-10 14:03:37 +1000

    Add files via upload

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ document.body.appendChild(canvasElement);
 
 
 // change to false to deploy
-export const LOCAL = false;
+export const LOCAL = true;
    
 const drawViz = (data) => { 
 document.body.innerHTML = "";
@@ -25,6 +25,11 @@ canvasElement.id = "container";
 var height = dscc.getHeight();
 var width = dscc.getWidth();
 
+// get default zoom Values for Deck GL 
+var zoomparameter =  data.style.zoomparameter.defaultValue
+var minZoomparameter =  data.style.minZoomparameter.defaultValue
+var maxZoomparameter =  data.style.maxZoomparameter.defaultValue
+
 // clear canvas does not work
 // ctx.clearRect(0, 0, width, height);
 // ctx.beginPath();   
@@ -35,24 +40,36 @@ var data3 = data2.replace(/\"]/g, "]");
 var data4 = data3.replace(/\["/g, "[");
 var data4 = JSON.parse(data4);
   // get intial Vaues for view
-var firstvalue = data1[0].coordinateid.toString();
-var coordinates = firstvalue.split(",");
-var longitudeView =parseFloat(coordinates[0]);
-var latitudeView =parseFloat(coordinates[1]);
+let startloop0 = 0;
+let startloop1 = 0;
+let nbritem = 0;
+data1.forEach((item) => {
+  var tt = item.coordinateid.toString();
+  var coordinatesx = tt.split(",");
+  var xxx =parseFloat(coordinatesx[0]);
+  var yyy =parseFloat(coordinatesx[1]);
+  startloop0=xxx+startloop0;
+  nbritem=nbritem+1;
+  startloop1=yyy+startloop1;
+});
+
+const longitudeView =startloop0/nbritem;
+const latitudeView =startloop1/nbritem;
+console.log(longitudeView,latitudeView,zoomparameter,minZoomparameter,maxZoomparameter)
   
   const INITIAL_VIEW_STATE = {
+    
     bearing: 0,
     longitude: longitudeView,
     latitude:  latitudeView,
-    zoom: 15,
-    minZoom: 5,
-    maxZoom: 20,
-    pitch: 40.5,
+    zoom: zoomparameter,
+    minZoom: minZoomparameter,
+    maxZoom: maxZoomparameter,
+    pitch: 42.5,
   };
 new Deck({
   width: width,
   height: height,
-  
   initialViewState: INITIAL_VIEW_STATE,
   controller: true,
   layers: [
@@ -61,8 +78,10 @@ new Deck({
       getPosition: d => d.coordinateid,
       getRadius: d => d.sizeid,
       getFillColor: d => d.colorid,
+      
     })
   ],
+  
 });
 };
 // renders locally
---
+++ localMessage.js
@@ -0,0 +1,58 @@
+/**
+ * This file provides the mock "data" received
+ * by your visualization code when you develop
+ * locally.
+ *
+ */
+export const message = {
+  tables: {
+    DEFAULT: [
+      {
+        coordinateid: ['20,54.1'],
+        colorid: ['250,0,0'],
+        sizeid: [20],
+      },
+      {
+        coordinateid: ['20,54.1'],
+        colorid: ['250,0,0'],
+        sizeid: [6],
+      },
+      {
+        coordinateid: ['20,54.1'],
+        colorid: ['250,0,0'],
+        sizeid: [6],
+      },
+    ],
+  },
+  fields: {
+    coordinateid: [
+      {
+        id: 'qt_nzqx6a0xvb',
+        name: 'Age Group',
+        type: 'TEXT',
+        concept: 'DIMENSION',
+      },
+    ],
+    colorid: [
+      {
+        id: 'qt_nzqx6a0xfb',
+        name: 'color',
+        type: 'TEXT',
+        concept: 'DIMENSION',
+      },
+    ],
+    sizeid: [
+      {
+        id: 'qt_8isx6a0xsb',
+        name: 'size',
+        type: 'NUMBER',
+        concept: 'METRIC',
+      },
+    ],
+  },
+  style: {"zoomparameter" : { "value": 5, "defaultValue": 5 },
+  "minZoomparameter" : { "value": 5, "defaultValue": 5 },
+  "maxZoomparameter" : { "value": 20, "defaultValue": 20 }},
+  
+   
+};
